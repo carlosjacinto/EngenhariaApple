bdb1e7d1e154d1d9017351cd698846adcb1cbf36
carlosjacinto <carlosjacinto@gec.inatel.br>
2017-10-02 20:51:15 -0300

    Alteracao Carlos 30-09 e Ximbinha = LOC = 0

--- Projeto/Java/src/control/Main.java
+++ Projeto/Java/src/control/Main.java
@@ -1,6 +1,6 @@
 package control;
 
-import view.PrincipalAdm;
+import view.Principal;
 
 public class Main {
 
@@ -10,7 +10,7 @@ public class Main {
 			com.birosoft.liquid.LiquidLookAndFeel.setLiquidDecorations(true, "mac");
 		} catch (Exception e) {
 		}
-		new PrincipalAdm().setVisible(true);
+		new Principal().setVisible(true);
 	}
 
 }
