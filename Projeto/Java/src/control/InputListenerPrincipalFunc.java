5186048fb6966ef7691de239ec2f0d8786dacfcf
carlosjacinto <carlosjacinto@gec.inatel.br>
2017-10-31 15:55:15 -0200

    Alteracao Carlos 22-10

--- Projeto/Java/src/control/InputListenerPrincipalFunc.java
+++ Projeto/Java/src/control/InputListenerPrincipalFunc.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 import view.ClienteView;
 import view.LoginView;
@@ -13,14 +14,14 @@ import view.PrincipalFunc;
 
 public class InputListenerPrincipalFunc implements MouseListener {
 	PrincipalFunc principalFunc;
-	
+
 	public InputListenerPrincipalFunc(PrincipalFunc principalFunc) {
 		this.principalFunc = principalFunc;
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getSource() == principalFunc.getSetaCima()) {
+		if (e.getSource() == principalFunc.getSetaCima()) {
 			principalFunc.getSetaCima().setVisible(false);
 			principalFunc.getBarraAtalhos().setVisible(true);
 			principalFunc.getBtPedido().setVisible(true);
@@ -29,7 +30,7 @@ public class InputListenerPrincipalFunc implements MouseListener {
 			principalFunc.getBtSair().setVisible(true);
 			principalFunc.getBtnDesligar().setVisible(true);
 			principalFunc.getSetaBaixo().setVisible(true);
-		}else if(e.getSource() == principalFunc.getSetaBaixo()) {
+		} else if (e.getSource() == principalFunc.getSetaBaixo()) {
 			principalFunc.getSetaCima().setVisible(true);
 			principalFunc.getBarraAtalhos().setVisible(false);
 			principalFunc.getBtPedido().setVisible(false);
@@ -39,90 +40,96 @@ public class InputListenerPrincipalFunc implements MouseListener {
 			principalFunc.getSetaBaixo().setVisible(false);
 			principalFunc.getBtnDesligar().setVisible(false);
 		}
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		if(e.getSource() == principalFunc.getBtCliente()) {
+		if (e.getSource() == principalFunc.getBtCliente()) {
 			principalFunc.getBtCliente().setIcon(new ImageIcon("Interno/cliente2x.png"));
-		}else if(e.getSource() == principalFunc.getBtPedido()) {
+		} else if (e.getSource() == principalFunc.getBtPedido()) {
 			principalFunc.getBtPedido().setIcon(new ImageIcon("Interno/pedidos2x.png"));
-		}else if(e.getSource() == principalFunc.getBtNotaEntrada()) {
+		} else if (e.getSource() == principalFunc.getBtNotaEntrada()) {
 			principalFunc.getBtNotaEntrada().setIcon(new ImageIcon("Interno/nota2x.png"));
-		}else if(e.getSource() == principalFunc.getBtSair()) {
+		} else if (e.getSource() == principalFunc.getBtSair()) {
 			principalFunc.getBtSair().setIcon(new ImageIcon("Interno/sair2x.png"));
-		}  
-		else if(e.getSource() == principalFunc.getBtnDesligar()) {
+		} else if (e.getSource() == principalFunc.getBtnDesligar()) {
 			principalFunc.getBtnDesligar().setIcon(new ImageIcon("Interno/sair2x.png"));
-		}  
-		
+		}
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		if(e.getSource() == principalFunc.getBtCliente()) {
+		if (e.getSource() == principalFunc.getBtCliente()) {
 			principalFunc.getBtCliente().setIcon(new ImageIcon("Interno/cliente.png"));
-		}else if(e.getSource() == principalFunc.getBtPedido()) {
+		} else if (e.getSource() == principalFunc.getBtPedido()) {
 			principalFunc.getBtPedido().setIcon(new ImageIcon("Interno/pedidos.png"));
-		}else if(e.getSource() == principalFunc.getBtNotaEntrada()) {
+		} else if (e.getSource() == principalFunc.getBtNotaEntrada()) {
 			principalFunc.getBtNotaEntrada().setIcon(new ImageIcon("Interno/nota.png"));
-		}else if(e.getSource() == principalFunc.getBtSair()) {
+		} else if (e.getSource() == principalFunc.getBtSair()) {
 			principalFunc.getBtSair().setIcon(new ImageIcon("Interno/sair.png"));
-		} else if(e.getSource() == principalFunc.getBtnDesligar()) {
+		} else if (e.getSource() == principalFunc.getBtnDesligar()) {
 			principalFunc.getBtnDesligar().setIcon(new ImageIcon("Interno/sair.png"));
-		}  
-		
+		}
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(e.getSource() == principalFunc.getBtCliente()) {
+		if (e.getSource() == principalFunc.getBtCliente()) {
 			principalFunc.getBtCliente().setIcon(new ImageIcon("Interno/cliente.png"));
 			new ClienteView().setVisible(true);
-		}else if(e.getSource() == principalFunc.getBtPedido()) {
+		} else if (e.getSource() == principalFunc.getBtPedido()) {
 			principalFunc.getBtPedido().setIcon(new ImageIcon("Interno/pedidos.png"));
 			new PedidoView().setVisible(true);
-		}else if(e.getSource() == principalFunc.getBtNotaEntrada()) {
+		} else if (e.getSource() == principalFunc.getBtNotaEntrada()) {
 			principalFunc.getBtNotaEntrada().setIcon(new ImageIcon("Interno/nota.png"));
 			new NotaEntradaView().setVisible(true);
-		}else if(e.getSource() == principalFunc.getBtSair()) {
+		} else if (e.getSource() == principalFunc.getBtSair()) {
 			principalFunc.getBtSair().setIcon(new ImageIcon("Interno/sair.png"));
 			principalFunc.dispose();
-			new LoginView().setVisible(true); 
-		}else if(e.getSource() == principalFunc.getmntmCliente()) {
+			new LoginView().setVisible(true);
+		} else if (e.getSource() == principalFunc.getmntmCliente()) {
 			new ClienteView().setVisible(true);
-		}else if(e.getSource() == principalFunc.getmntmPedidos()) {
+		} else if (e.getSource() == principalFunc.getmntmPedidos()) {
 			new PedidoView().setVisible(true);
-		}else if(e.getSource() == principalFunc.getmntmNotaEntrada()) {
+		} else if (e.getSource() == principalFunc.getmntmNotaEntrada()) {
 			new NotaEntradaView().setVisible(true);
-		}  else if(e.getSource() == principalFunc.getBtnDesligar()) {
+		} else if (e.getSource() == principalFunc.getBtnDesligar()) {
 			principalFunc.getBtnDesligar().setIcon(new ImageIcon("Interno/sair.png"));
 			principalFunc.dispose();
-		}else if(e.getSource() == principalFunc.getmntmDesligar()) {
-			principalFunc.dispose();
+		} else if (e.getSource() == principalFunc.getmntmDesligar()) {
+			int result = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja realmente sair da aplicação?", "Sair",
+					JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				principalFunc.dispose();
+			}
+		} else if (e.getSource() == principalFunc.getmntmSair()) {
+			int result = JOptionPane.showConfirmDialog(null, "Deseja realmente sair da sessão usuário?", "Sair",
+					JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				principalFunc.dispose();
+				new LoginView().setVisible(true);
+			}
 		}
-		else if(e.getSource() == principalFunc.getmntmSair()) {
-			principalFunc.dispose();
-			new LoginView().setVisible(true); 
-		}
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(e.getSource() == principalFunc.getBtCliente()) {
+		if (e.getSource() == principalFunc.getBtCliente()) {
 			principalFunc.getBtCliente().setIcon(new ImageIcon("Interno/cliente2x.png"));
-		}else if(e.getSource() == principalFunc.getBtPedido()) {
+		} else if (e.getSource() == principalFunc.getBtPedido()) {
 			principalFunc.getBtPedido().setIcon(new ImageIcon("Interno/pedidos2x.png"));
-		}else if(e.getSource() == principalFunc.getBtNotaEntrada()) {
+		} else if (e.getSource() == principalFunc.getBtNotaEntrada()) {
 			principalFunc.getBtNotaEntrada().setIcon(new ImageIcon("Interno/nota2x.png"));
-		}else if(e.getSource() == principalFunc.getBtSair()) {
+		} else if (e.getSource() == principalFunc.getBtSair()) {
 			principalFunc.getBtSair().setIcon(new ImageIcon("Interno/sair2x.png"));
-		}  else if(e.getSource() == principalFunc.getBtnDesligar()) {
+		} else if (e.getSource() == principalFunc.getBtnDesligar()) {
 			principalFunc.getBtnDesligar().setIcon(new ImageIcon("Interno/sair2x.png"));
-		}  
-		
+		}
+
 	}
 
 }
--- Projeto/Java/src/control/Main.java
+++ Projeto/Java/src/control/Main.java
@@ -1,6 +1,6 @@
 package control;
 
-import view.NotaEntradaView;
+import view.Principal;
 
 public class Main {
 
@@ -11,6 +11,6 @@ public class Main {
 		} catch (Exception e) {
 		}
 		
-		new NotaEntradaView().setVisible(true);
+		new Principal().setVisible(true);
 	}
 }
