691b5bf05e026833e0cdb0421b8a5bba71ed0b6b
carlosjacinto <carlosjacinto@gec.inatel.br>
2017-11-27 15:04:12 -0200

    Acabou

--- Projeto/Java/src/control/Main.java
+++ Projeto/Java/src/control/Main.java
@@ -1,6 +1,6 @@
 package control;
 
-import view.PrincipalAdm;
+import view.Principal;
 
 
 public class Main {
@@ -11,6 +11,6 @@ public class Main {
 			com.birosoft.liquid.LiquidLookAndFeel.setLiquidDecorations(true, "mac");
 		} catch (Exception e) {
 		}
-		new PrincipalAdm().setVisible(true);
+		new Principal().setVisible(true);
 	}
 }
